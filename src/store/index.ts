d259a3b9fec7ca020faeb03dcdd0636cb894d416
arturfv <artur.fesenko@auto.ria.com>
2024-09-21 00:19:36 +0300

    refactored

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import categoriesReducer from "../slices/categorySlices";
 import questionsReducer from "../slices/questionsSlices";
-import examReducer from "../slices/examSlices";
 
 export const store = configureStore({
     reducer: {
         categories: categoriesReducer,
         questions: questionsReducer,
-        exam: examReducer,
     },
 });
 
